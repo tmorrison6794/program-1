62634ca6073f5f59fffef67a1276ecda05ead93a
Thomas Morrison <tmorrison6794@sdsu.edu>
2026-02-24 14:14:25 -0800

    Commit #2 Febuary 24th

--- cmake-build-debug/program_1.cpp
+++ cmake-build-debug/program_1.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include<ctime>
+using namespace std;
 
-int main() {
-    std::cout << "Hello World" << std::endl;
-}
+
+static const int MAX_SPACES=40;
+
+
+class MonopolySpace {
+    public:
+    string propertyName;
+    string propertyColor;
+    int value;
+    int rent;
+
+    MonopolySpace() {
+        propertyName="";
+        propertyColor="";
+        value=0;
+        rent=0;
+    }
+    MonopolySpace(string propertyName,string propertyColor,int value, int rent) {
+        this->propertyName=propertyName;
+        this->propertyColor=propertyColor;
+        this->rent=rent;
+        this->value=value;
+    }
+    bool isEqual(MonopolySpace other) {
+        return propertyName==other.propertyName && propertyColor==other.propertyColor && rent==other.rent;
+    }
+
+};
